a25f2771435cc3dca8b1086ebb458a70df7482d0
SrgeiHein <hein@nexbe.sg>
2025-10-02 01:21:36 +0700

    update portfolio

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,25 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, url }) => {
+  const card = (
+    <div className="proj-imgbx">
+      <img src={imgUrl} alt={`${title} screenshot`} />
+      <div className="proj-txtx">
+        <h4>{title}</h4>
+        <span>{description}</span>
+      </div>
+    </div>
+  );
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
+      {url ? (
+        <a className="project-link" href={url} target="_blank" rel="noreferrer">
+          {card}
+        </a>
+      ) : (
+        card
+      )}
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,12 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/aperiaProject.jpg";
-import projImg2 from "../assets/img/nextimeProject.png";
+import projImg2 from "../assets/img/nexStack.png";
 import projImg3 from "../assets/img/Obenergy.jpg";
+import projImg4 from "../assets/img/globotix.sg.png";
+import projImg5 from "../assets/img/arvas.png";
+import projImg6 from "../assets/img/anaca project.png";
+import projImg7 from "../assets/img/RenoQuote project.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -13,18 +17,58 @@ export const Projects = () => {
       title: "Aperia Website",
       description: "Website Development",
       imgUrl: projImg1,
+      url: "https://aperia.ai/",
     },
     {
-      title: "nexTime Webiste",
+      title: "nexStack Website",
       description: "Saas Development",
       imgUrl: projImg2,
+      url: "https://nexstack.sg/",
     },
     {
       title: "OneBerry Website",
       description: "Energy Management Development",
       imgUrl: projImg3,
+      url: "https://web.neahecs.com/",
+    },
+    {
+      title: "Globotix Website",
+      description: "Website Development",
+      imgUrl: projImg4,
+      url: "https://www.globotix.sg/",
+    },
+    {
+      title: "Arvas AI Website",
+      description: "Website Development",
+      imgUrl: projImg5,
+      url: "https://arvas.ai/",
+    },
+    {
+      title: "Anaca Website",
+      description: "Website Development",
+      imgUrl: projImg6,
+      url: "https://anaca.sg/",
+    },
+    {
+      title: "RenoQuotez Website",
+      description: "Website Development",
+      imgUrl: projImg7,
+      url: "https://renoquotez.singaporetestlab.com/",
     },
   ];
+
+  const projectsPerTab = 3;
+  const tabKeys = ["first", "second", "third"];
+  const projectGroups = tabKeys.map((_, tabIndex) => {
+    if (projects.length === 0) {
+      return [];
+    }
+    return Array.from({ length: projectsPerTab }, (_, offset) => {
+      const projectIndex =
+        (tabIndex * projectsPerTab + offset) % projects.length;
+      return projects[projectIndex];
+    });
+  });
 
   return (
     <section className="project" id="projects">
@@ -52,15 +96,13 @@ export const Projects = () => {
                       className="nav-pills mb-5 justify-content-center align-items-center"
                       id="pills-tab"
                     >
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                      </Nav.Item>
+                      {tabKeys.map((eventKey, index) => (
+                        <Nav.Item key={eventKey}>
+                          <Nav.Link eventKey={eventKey}>{`Tab ${
+                            index + 1
+                          }`}</Nav.Link>
+                        </Nav.Item>
+                      ))}
                     </Nav>
                     <Tab.Content
                       id="slideInUp"
@@ -68,31 +110,20 @@ export const Projects = () => {
                         isVisible ? "animate__animated animate__slideInUp" : ""
                       }
                     >
-                      <Tab.Pane eventKey="first">
-                        <Row>
-                          {projects.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
-                        </Row>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="section">
-                        <p>
-                          The following projects illustrate my hands-on
-                          experience with web technologies, including front-end
-                          and back-end development. Each project represents my
-                          expertise in delivering high-quality solutions
-                          tailored to specific business needs.
-                        </p>
-                      </Tab.Pane>
-                      <Tab.Pane eventKey="third">
-                        <p>
-                          The following projects illustrate my hands-on
-                          experience with web technologies, including front-end
-                          and back-end development. Each project represents my
-                          expertise in delivering high-quality solutions
-                          tailored to specific business needs.
-                        </p>
-                      </Tab.Pane>
+                      {tabKeys.map((eventKey, tabIndex) => (
+                        <Tab.Pane eventKey={eventKey} key={eventKey}>
+                          <Row>
+                            {projectGroups[tabIndex].map(
+                              (project, cardIndex) => (
+                                <ProjectCard
+                                  key={`${eventKey}-${cardIndex}-${project.title}`}
+                                  {...project}
+                                />
+                              )
+                            )}
+                          </Row>
+                        </Tab.Pane>
+                      ))}
                     </Tab.Content>
                   </Tab.Container>
                 </div>
